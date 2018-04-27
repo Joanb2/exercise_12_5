cadc30367eed082fd4a36ffd33602a953ce84c0a
Joanna Buchała <joanna.b2@op.pl>
2018-04-27 21:02:53 +0200

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,31 @@
-var url='http://api.icndb.com/jokes/random';
-
-var $button  = $('#get-joke').on('click', function() {
-	getJoke();
-});
-
-var $paragraph = $('#joke');
-
-function getJoke(){
-	$.ajax({
-		method: 'GET',
-		url: url,
-		success: function(res) {
-			$paragraph.text(res.value.joke);
-		}
-
-	});
-}
-var myweb = "https://joanb2.github.io/exercise_12_5/";
-var prefix = "https://cors-anywhere.herokuapp.com/";
-var tweetLink = "https://twitter.com/intent/tweet?text=";
-var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-
 function getQuote() {
 	$.ajax({
 	    dataType: "json",
-	    url: quoteUrl + prefix,
+	    url: prefix + quoteUrl,
 	    data: myweb,
-	    success: createTweet,
-	    $.ajaxSetup({ cache: false });
+	    success: createTweet
 	});
+
+	$.ajaxSetup({ cache: false });
 }
 
 function createTweet(input) {
 	var data = input[0];
-	var quoteText = $(data.content).text().trim();
-	var quoteAuthor = data.title;
+    var quoteText = $(data.content).text().trim();
+    var quoteAuthor = data.title;
 
-	if (!quoteAuthor.lenght) {
-		quoteAuthor = "Unknow author";
-	}
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
+	var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
 
-	var tweetText = "Quote of the day -" + quoteText + "Author: " + quoteAuthor;
-
-	if (tweetText.lenght > 140) {
-		getQuote();
-	}	else {
-		var tweet = tweetLink + encodeURIComponent(tweetText);
-		$('.quote').text(quoteText);
-		$('.author').text("Author: " + quoteAuthor);
-		$('.tweet').attr('href', tweet);
+	if (tweetText.length > 140) {
+    	getQuote();
+	} else {
+	    var tweet = tweetLink + encodeURIComponent(tweetText);
+	    $('.quote').text(quoteText);
+	    $('.author').text("Author: " + quoteAuthor);
+	    $('.tweet').attr('href', tweet);
 	}
 }
 
